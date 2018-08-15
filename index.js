d61ca80dea48083e39cf61ba10b0e8c90824817d
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-15 21:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(name, katzDeliLine){
-katzDeliLine.push(`${name}`)
-return( `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+  katzDeliLine.push(`${name}`)
+  return( `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
